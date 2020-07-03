3c1947d1cd646dd1755ae3bc0c951f59661a1b65
Malachi Soord <inverse.chi@gmail.com>
2020-07-03 14:13:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tell_me_your_secrets_tmp',
+    name='tell_me_your_secrets',
     packages=setuptools.find_packages(),
     version='1.74',      
     license='MIT',
