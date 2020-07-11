50c10bf89139d2d0ada1afe2417ed76b96436349
Malachi Soord <inverse.chi@gmail.com>
2020-07-11 21:04:59 +0200

    Minor improvements (#4)
    * Add some improvements
    
    * Additional log
    
    * Improve help
    
    * Reduce nesting
    
    * Improve logging
    
    * Fix type hints
    
    * Simplify logger with f string
    
    * Improve scripts
    
    * Update setup.py
    
    * Incremented Version number.
    
    Based on changes suggested in #3

--- tell_me_your_secrets/__main__.py
+++ tell_me_your_secrets/__main__.py
@@ -1,15 +1,13 @@
 import yaml
-import os
 import abc
 from .defaults import *
 from .logger import create_logger,logging
 from .utils import *
 import re
 from pandas import DataFrame
-import datetime
 import argparse
 import sys
-import functools
+
 
 config_names = col_print('Available Signatures : \n',AVAILABLE_NAMES,COL_PRINT_WIDTH)
 
@@ -18,7 +16,7 @@ Tell Me Your Secrets
 
 Finds presence of secret files from lots of know signatures for a given folder path. 
 
-This module can be potentially be used to find secrets accross the Linux systems
+This module can be potentially be used to find secrets across the Linux systems
 
 {config_names}
 
@@ -35,8 +33,9 @@ arguement_parser.add_argument('search_path',help='The Root Directory From which 
 arguement_parser.add_argument('-c','--config',help='Path To Another config.yml for Extracting The Data')
 arguement_parser.add_argument('-w','--write',help='Path of the csv File to which results are written')
 arguement_parser.add_argument('-f','--filter',help='Filter the Signatures you want to apply. ',nargs='+')
+arguement_parser.add_argument('-v','--verbose',help='Enable debug level logging. ', action='store_true')
 
-module_logger = create_logger(MODULE_NAME,level=logging.INFO)
+module_logger = create_logger(MODULE_NAME)
 
 # Options
     # --path <PATH>: Checks Signatures in the Path.
@@ -61,7 +60,7 @@ class Signature(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def match(self, file_path,file_content) -> bool:
         """Match Input of the With Signature of the part and Signature and Type of matching."""
-        return
+        raise NotImplemented
 
 
 class RegexSignature(Signature):
@@ -80,14 +79,13 @@ class RegexSignature(Signature):
         elif self.part == 'path':
             compare_variable = file_path
         else:
-            module_logger.warn('Unrecognised File Part Access %s',self.name)
+            module_logger.warn(f'Unrecognised File Part Access {self.name}')
             return False
 
         return self.regex.search(compare_variable) != None
     
     def __str__(self):
         return self.name + ' : ' + self.part + ' : Regex : '+ self
-
 
 
 class SimpleMatch(Signature):
@@ -105,21 +103,20 @@ class SimpleMatch(Signature):
         elif self.part == 'path':
             compare_variable = file_path
         else:
-            module_logger.warn('Unrecognised File Part Access %s',self.name)
+            module_logger.warn(f'Unrecognised File Part Access {self.name}')
         
         return compare_variable == self.signature
 
 
 class SignatureRecognizer:
-    def __init__(self,config_object,path,print_results=VERBOSE_OUTPUT,write_results=SAVE_ON_COMPLETE,output_path=DEFAULT_OUTPUT_PATH,user_filters=[]):
-        self.config = config_object # todo: Fix this Later. 
+    def __init__(self, config_object: dict, path, print_results=VERBOSE_OUTPUT, write_results=SAVE_ON_COMPLETE,
+                 output_path=DEFAULT_OUTPUT_PATH, user_filters=[]):
+        self.config = config_object  # todo: Fix this Later.
         self.path = path
-        self.blacklisted_extensions = config_object['blacklisted_extensions']
+        self.blacklisted_extensions = config_object.get('blacklisted_extensions', [])
         self.blacklisted_paths = [path.format(sep=os.path.sep) for path in config_object['blacklisted_paths']]
-        self.red_flag_extensions = config_object['red_flag_extensions']
-        self.max_file_size = MAX_FILE_SIZE
-        if 'max_file_size' in config_object:
-            self.max_file_size = config_object['max_file_size']
+        self.red_flag_extensions = config_object.get('red_flag_extensions', [])
+        self.max_file_size = config_object.get('max_file_size', MAX_FILE_SIZE)
         self.write_results = write_results
         self.print_results = print_results
         self.signatures = []
@@ -128,7 +125,7 @@ class SignatureRecognizer:
         self.output_path = output_path
         # $ Make Configuration Objects For each of the Signatures in the Config Object.
         self.load_config()
-        module_logger.info('Secret Sniffer Initailsed For Path  : %s\n',path)
+        module_logger.info(f'Secret Sniffer Initialised For Path: {path}')
 
     # $ Create the signature objects over here. 
     def load_config(self):
@@ -145,12 +142,12 @@ class SignatureRecognizer:
             elif 'regex' in signature_obj:
                 self.signatures.append(RegexSignature(signature_obj['part'],signature_obj['name'],signature_obj['regex']))
             else:
-                module_logger.warn('No Match Method Of Access %s',self.name)
+                module_logger.warn(f'No Match Method Of Access')
             chosen_configs.append(signature_obj['name']+" In File "+signature_obj['part'])
         
         if len(self.user_filters) > 0:
             module_logger.info('Applying Filtered Signatures : \n\n\t%s\n','\n\t'.join(chosen_configs))
-    
+
     def create_matched_signature_object(self,name,part,file_path):
         return {
             'name': name,
@@ -163,18 +160,20 @@ class SignatureRecognizer:
         for possible_compromised_path in filtered_files:
             # $ todo : Create more modular processing of files. 
             # $ todo : Create a threaded version of the processing of files
-            # module_logger.debug("Opening File : %s",possible_compromised_path)
+            module_logger.debug(f'Opening File : {possible_compromised_path}')
             file_content = get_file_data(possible_compromised_path)
             if file_content is None:
                 continue
             # $ Run the Signature Checking Engine over here For different Pattern Signatures. 
             signature_name,signature_part = self.run_signatures(possible_compromised_path,file_content)
             if signature_name is not None:
-                self.matched_signatures.append(self.create_matched_signature_object(signature_name,signature_part,possible_compromised_path))
+                self.matched_signatures.append(self.create_matched_signature_object(signature_name, signature_part,
+                                                                                    possible_compromised_path))
                 if self.print_results:
-                    module_logger.info('Signature Matched : %s | On Part : %s | With File : %s',signature_name,signature_part,possible_compromised_path)
+                    module_logger.info(f'Signature Matched : {signature_name} | On Part : {signature_part} | With '
+                                       f'File : {possible_compromised_path}')
 
-        module_logger.info("Found %d Matches from the Path %s",len(self.matched_signatures),self.path)
+        module_logger.info(f'Found {len(self.matched_signatures)} matches from the path {self.path}')
         if self.write_results:
             self.write_results_to_file()
     
@@ -185,9 +184,8 @@ class SignatureRecognizer:
                 self.output_path+='.csv'
             file_name = self.output_path
             write_df.to_csv(file_name)
-            module_logger.info('Completed Writing Results to File : %s',self.output_path)
-            
-    
+            module_logger.info(f'Completed Writing Results to File : {self.output_path}')
+
     def run_signatures(self,file_path,content):
         for signature in self.signatures:
             match_result = signature.match(file_path,content)
@@ -230,40 +228,48 @@ class SignatureRecognizer:
         return f
 
     
-def init_signature(config,path,write_path,user_filters):
+def init_signature(config: dict, path: str, write_path: str, user_filters: list):
     # $ todo : Create the signature Object with the methods that
-    if write_path is None:
-        return SignatureRecognizer(config,path,user_filters=user_filters)
-    else:
-        return SignatureRecognizer(config,path,write_results=True,output_path=write_path,user_filters=user_filters)
+    if write_path:
+        return SignatureRecognizer(config, path, write_results=True, output_path=write_path, user_filters=user_filters)
+
+    return SignatureRecognizer(config, path, user_filters=user_filters)
 
 # $ Gets all subpaths for the directory. 
 
 
 def run_service():
     # $ todo : Import Config.yml or Use the one From defaults. 
-    parsed_arguements = arguement_parser.parse_args()
-    config_path = DEFAULT_CONFIG_PATH
-    if parsed_arguements.config is not None:
-        config_path = os.path.abspath(os.path.join(os.path.abspath(sys.path[0]),os.path.abspath(parsed_arguements.config)))
+    parsed_arguments = arguement_parser.parse_args()
 
-    search_path =  os.path.abspath(os.path.join(os.path.abspath(sys.path[0]),os.path.abspath(parsed_arguements.search_path)))
-    module_logger.debug('Running Config From Path : %s',config_path)
-    f = open(config_path)
-    config = yaml.load(f,Loader=yaml.FullLoader)
-    f.close()
-    
+    if parsed_arguments.verbose:
+        module_logger.setLevel(logging.DEBUG)
+        module_logger.handlers[0].setLevel(logging.DEBUG)
+
+    module_logger.debug(f'Parsed arguments {parsed_arguments}')
+
+    config_path = DEFAULT_CONFIG_PATH
+    if parsed_arguments.config is not None:
+        config_path = os.path.abspath(os.path.join(os.path.abspath(sys.path[0]),os.path.abspath(parsed_arguments.config)))
+
+    search_path = os.path.abspath(os.path.join(os.path.abspath(sys.path[0]),os.path.abspath(parsed_arguments.search_path)))
+    module_logger.debug(f'Running config from path: {config_path}')
+
+    with open(config_path) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    module_logger.debug(f'Config contents: \n{config}')
+
     write_path = None
-    if parsed_arguements.write is not None:
-        write_path = os.path.abspath(os.path.join(os.path.abspath(sys.path[0]),os.path.abspath(parsed_arguements.write)))
+    if parsed_arguments.write is not None:
+        write_path = os.path.abspath(os.path.join(os.path.abspath(sys.path[0]), os.path.abspath(parsed_arguments.write)))
     
     user_filters = []
-    if parsed_arguements.filter is not None:
-        user_filters = parsed_arguements.filter
+    if parsed_arguments.filter is not None:
+        user_filters = parsed_arguments.filter
 
-    # print(parsed_arguements)
-    # $  Extract FILTERED Files from the Path 
-    sig_recognizer = init_signature(config,search_path,write_path,user_filters)
+    # $  Extract FILTERED Files from the Path
+    sig_recognizer = init_signature(config, search_path, write_path, user_filters)
     sig_recognizer.find_vulnerable_files()
     
 
--- tell_me_your_secrets/logger.py
+++ tell_me_your_secrets/logger.py
@@ -1,21 +1,13 @@
 import logging
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
 
-
-def create_logger(logger_name:str,level=logging.INFO):
+def create_logger(logger_name: str, level: int = logging.INFO):
     custom_logger = logging.getLogger(logger_name)
-    if level != logging.INFO:
-        ch1 = logging.StreamHandler()
-        ch1.setLevel(level)
-        ch1.setFormatter(formatter)
-        custom_logger.addHandler(ch1)
-        custom_logger.setLevel(level)
-    else:
-        custom_logger.addHandler(ch)
-        custom_logger.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    custom_logger.addHandler(ch)
+    custom_logger.setLevel(level)
         
     return custom_logger
