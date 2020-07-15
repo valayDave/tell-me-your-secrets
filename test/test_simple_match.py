0a64ba75e3183019ea708b62d6cc8733c646ff82
Malachi Soord <inverse.chi@gmail.com>
2020-07-15 21:46:23 +0200

    Readability

--- test/test_simple_match.py
+++ test/test_simple_match.py
@@ -9,40 +9,40 @@ class SimpleMatchTest(unittest.TestCase):
     @patch('tell_me_your_secrets.__main__.find_extension')
     def test_match_extension_match(self, mock_find_extension):
         mock_find_extension.return_value = '.pem'
-        match = SimpleMatch('extension', 'Potential cryptographic private key', '.pem')
-        self.assertTrue(match.match('/path/to/pem', ''))
+        simple_match = SimpleMatch('extension', 'Potential cryptographic private key', '.pem')
+        self.assertTrue(simple_match.match('/path/to/pem', ''))
 
     @patch('tell_me_your_secrets.__main__.find_extension')
     def test_match_extension_no_match(self, mock_find_extension):
         mock_find_extension.return_value = '.jpg'
-        match = SimpleMatch('extension', 'Potential cryptographic private key', '.pem')
-        self.assertFalse(match.match('/path/to/no/pem', ''))
+        simple_match = SimpleMatch('extension', 'Potential cryptographic private key', '.pem')
+        self.assertFalse(simple_match.match('/path/to/no/pem', ''))
 
     def test_match_filename_match(self):
-        match = SimpleMatch('filename', 'Ruby On Rails secret token configuration file', 'secret_token.rb')
-        self.assertTrue(match.match('/path/to/secret_token.rb', ''))
+        simple_match = SimpleMatch('filename', 'Ruby On Rails secret token configuration file', 'secret_token.rb')
+        self.assertTrue(simple_match.match('/path/to/secret_token.rb', ''))
 
     def test_match_filename_no_match(self):
-        match = SimpleMatch('filename', 'Ruby On Rails secret token configuration file', 'secret_token.rb')
-        self.assertFalse(match.match('/path/to/non_secret_token.rb', ''))
+        simple_match = SimpleMatch('filename', 'Ruby On Rails secret token configuration file', 'secret_token.rb')
+        self.assertFalse(simple_match.match('/path/to/non_secret_token.rb', ''))
 
     def test_match_contents_match(self):
-        match = SimpleMatch('contents', 'Sample content match', 'abcdef')
-        self.assertTrue(match.match('/path/to/file', 'abcdef'))
+        simple_match = SimpleMatch('contents', 'Sample content match', 'abcdef')
+        self.assertTrue(simple_match.match('/path/to/file', 'abcdef'))
 
     def test_match_contents_no_match(self):
-        match = SimpleMatch('contents', 'Sample content match', 'abcdef')
-        self.assertFalse(match.match('/path/to/file', 'xyz'))
+        simple_match = SimpleMatch('contents', 'Sample content match', 'abcdef')
+        self.assertFalse(simple_match.match('/path/to/file', 'xyz'))
 
     def test_match_path_match(self):
-        match = SimpleMatch('path', 'Sample path match', '/path/to/secret')
-        self.assertTrue(match.match('/path/to/secret', ''))
+        simple_match = SimpleMatch('path', 'Sample path match', '/path/to/secret')
+        self.assertTrue(simple_match.match('/path/to/secret', ''))
 
     def test_match_path_no_match(self):
-        match = SimpleMatch('path', 'Sample path match', '/path/to/secret')
-        self.assertFalse(match.match('/path/to/no_secret', ''))
+        simple_match = SimpleMatch('path', 'Sample path match', '/path/to/secret')
+        self.assertFalse(simple_match.match('/path/to/no_secret', ''))
 
     def test_match_str(self):
-        match = SimpleMatch('Part', 'Name ', 'Sig')
+        simple_match = SimpleMatch('Part', 'Name ', 'Sig')
         self.assertEqual("Type:<class 'tell_me_your_secrets.__main__.SimpleMatch'> Name:Name  Part:Part: Signature:Sig",
-                         str(match))
+                         str(simple_match))
